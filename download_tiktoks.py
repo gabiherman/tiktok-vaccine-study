8250790d28b56edf7655640d23aa7b4e822017cf
gabiherman <39273726+gabiherman@users.noreply.github.com>
2021-07-01 21:05:08 -0400

    the code seems to work!

--- download_tiktoks.py
+++ download_tiktoks.py
@@ -1,6 +1,7 @@
 import random
 from TikTokApi import TikTokApi
 import pandas as pd
+import json
 
 #extract metadata from tiktok dict t, download author dict for follower count
 def extract_metadata(t):
@@ -10,9 +11,13 @@ def extract_metadata(t):
     shares=str(t['stats']['shareCount'])
     comments=str(t['stats']['commentCount'])
     plays=str(t['stats']['playCount'])
+    desc=str(t['desc'])
     url= 'https://www.tiktok.com/@'+author+'/video/'+tiktok_id
-    author_info = api.get_user(author)
-    followers = author_info['userInfo']['stats']['followerCount']
+    followers="NULL"
+    if author!="internalmeddoctor": #this account seems to have been deleted and is gumming things up - hackiest fix possible
+        print("author ok")
+        author_info = api.get_user(author)
+        followers = author_info['userInfo']['stats']['followerCount']
     dct= {
         'author':author,
         'url':url,
@@ -20,30 +25,35 @@ def extract_metadata(t):
         'shares':shares,
         'comments':comments,
         'author_followers':followers,
-        'plays': plays
+        'plays': plays,
+        'description':desc
     }
     return dct
 
 
-
+#hashtags can be found in 'desc'
 def download_tiktok(url):
     data=api.get_video_by_url(dgfsgdfg)
     with open("video.mp4", "wb") as out:
         out.write(data)
 
 
+verifyFp="verify_kqlmgn2f_sgABDdgW_KdLv_46hN_87VW_PRVAVQvPspDs"
+api = TikTokApi.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
 
+tiktoks = api.byHashtag("covidvaccine", count=150,custom_verifyFp=verifyFp)
 
-api = TikTokApi.get_instance()
-c=20
-tiktoks = api.byHashtag("covidvaccine", count=c)
+with open('./out/all_dicts.json', 'w') as f:
+    json.dump(tiktoks, f, indent=True)
 dat=[]
 for t in tiktoks:
     dat.append(extract_metadata(t))
 
 df = pd.DataFrame(dat)
-df.to_csv('./out/sample_df.csv')
+df.to_csv('./out/tiktok_metadata_07-01-2021_2055.csv')
 print("success")
+
+
 
 
 #df = pd.json_normalize(d, sep='_')
--- test_script.py
+++ test_script.py
@@ -1,11 +1,13 @@
 import random
 from TikTokApi import TikTokApi
-api = TikTokApi.get_instance()
+verifyFp="verify_kqlmgn2f_sgABDdgW_KdLv_46hN_87VW_PRVAVQvPspDs"
+api = TikTokApi.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
+
 
 #i don't think I need this if I'm not downloading?
 #custom_did = str(random.randint(10000, 99999999))
 #pull the tiktoks
-#tiktoks = api.byHashtag("covidvaccine", count=10)
+tiktoks = api.byHashtag("covidvaccine", count=150,custom_verifyFp=verifyFp)
 
 #def construct_tiktok_url(t):
 #    author=str(t['author']['uniqueId'])
@@ -18,7 +20,7 @@ api = TikTokApi.get_instance()
 #for follower count it's api.get_user[stats][followerCount]
 
 #print(tiktoks[0].keys())
-#print(tiktoks[0]['author'])
+print(tiktoks[0])
 
 #url is https://www.tiktok.com/@[['author']['uniqueId']]/video/['id']
 
@@ -27,8 +29,8 @@ api = TikTokApi.get_instance()
 #    out.write(data)
 
 
-author_info = api.get_user('brandongrogan61')
-print(author_info.keys())
-followers = author_info['userInfo']['stats']['followerCount']
+#author_info = api.get_user('brandongrogan61')
+#print(author_info.keys())
+#followers = author_info['userInfo']['stats']['followerCount']
 
-print(followers)
+#print(followers)
